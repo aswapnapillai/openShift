74dd5e08dc25370fa5e6c10a4a2a3eff160f968c
swapna <swara@ikea.com>
2020-04-01 01:18:26 +0530

    test cases

--- bookrecommendationservice/src/main/java/com/stackroute/bookrecommendationservice/config/Consumer.java
+++ bookrecommendationservice/src/main/java/com/stackroute/bookrecommendationservice/config/Consumer.java
@@ -5,8 +5,8 @@ import com.stackroute.bookrecommendationservice.dao.BookDetails;
 import com.stackroute.bookrecommendationservice.exception.BookRecommendationNotAddedException;
 import com.stackroute.bookrecommendationservice.service.BookRecommendationService;
 import com.stackroute.bookrecommendationservice.rabbitmq.domain.UserDTO;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+//import org.springframework.amqp.rabbit.annotation.RabbitListener;
+//import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,7 +21,7 @@ public class Consumer {
     @Autowired
     private BookRecommendationService bookRecommendationServiceImpl;
 
-    @RabbitListener(queues = "user_fav_ book_queue")
+   // @RabbitListener(queues = "user_fav_ book_queue")
    public void getUserDtoFromRabbitMq(BookDetails bookDetails, String user) throws BookRecommendationNotAddedException{
     	 bookRecommendationServiceImpl.saveRecommendation(bookDetails,user);
 
